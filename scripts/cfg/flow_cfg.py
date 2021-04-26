275059c830e64176b68b41b5487a98c367540dbe
ll574918628 <574918628@qq.com>
2021-04-26 08:01:01 +0000

    foundry

--- scripts/cfg/flow_cfg.py
+++ scripts/cfg/flow_cfg.py
@@ -20,4 +20,4 @@ design_b_top    = Flow('DESIGN_B_TOP','smic55','HD','MAX')
 design_b_crg    = Flow('design_b_crg','smic55','HD','MAX')
 coyote_tc       = Flow('coyote_tc','sky130','HS','TYP')
 aes             = Flow('aes_cipher_top','sky130','HS','TYP')
-
+Top             = Flow('Top','sky130','HS','TYP')
