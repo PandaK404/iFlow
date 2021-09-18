491f1220446a03e911feb704900ff15506104784
ZhishengZeng <ZhishengZeng@163.com>
2021-09-18 16:48:08 +0800

    update

--- build_iflow.sh
+++ build_iflow.sh
@@ -4,7 +4,7 @@
  # @Date: 2021-09-18 14:23:10
  # @Description: 
  # @LastEditors: Zhisheng Zeng
- # @LastEditTime: 2021-09-18 16:15:00
+ # @LastEditTime: 2021-09-18 16:46:33
  # @FilePath: /iFlow/build_iflow.sh
 ### 
 # env
@@ -15,6 +15,7 @@ IFLOW_TOOLS_DIR=$(cd "$(dirname "$0")" && pwd)/tools
 
 export IFLOW_BUILD_THREAD_NUM
 export IFLOW_ROOT_DIR
+export IFLOW_SHELL_DIR
 export IFLOW_TOOLS_DIR
 
 source $IFLOW_SHELL_DIR/common.sh
